b01af232f30960a5dbfae4fd46c2b9e068e9c397
fl0werpowers <47599466+fl0werpowers@users.noreply.github.com>
2024-03-22 02:36:53 +0100

    Implement all the necessary components

--- bannerInfo.js
+++ bannerInfo.js
@@ -9,11 +9,6 @@ async function fetchSiteTitle() {
   const request = await fetch(`https://nekoweb.org/api/site/info/${username}`);
   const siteInfoJson = await request.json();
 
-  if (document.getElementById("NOTUSEDONNEKOWEBsitelink")) {
-    document.getElementById(
-      "NOTUSEDONNEKOWEBsitelink"
-    ).innerHTML = `${username}.nekoweb.org`;
-  }
   if (document.getElementById("NOTUSEDONNEKOWEBsitetitle")) {
     document.getElementById(
       "NOTUSEDONNEKOWEBsitetitle"
@@ -21,14 +16,36 @@ async function fetchSiteTitle() {
   }
 }
 
-fetchSiteTitle();
+function fillUserSiteboxData() {
+  document.getElementById(
+    "NOTUSEDONNEKOWEBsitelink"
+  ).innerHTML = `${username}.nekoweb.org <span
+  style="position: relative !important"
+  class="follow"
+  >[+]</span
+>`;
+  document.getElementById(
+    "NOTUSEDONNEKOWEBanchor"
+  ).href = `https://${username}.nekoweb.org/`;
+  document.getElementById(
+    "NOTUSEDONNEKOWEBsitescreenshot"
+  ).src = `https://nekoweb.org/screenshots/${username}/index_large.jpg`;
+}
 
-function fetchSiteCSS() {
+function fillSiteboxCSS() {
   const stylesheet = `https://${username}.nekoweb.org/elements.css`;
 
   const cssId = "userCss";
 
   if (!document.getElementById(cssId)) {
-    const head = document.getElementsByTagName("head")[0];
+    const link = document.createElement("link");
+    link.id = cssId;
+    link.rel = "stylesheet";
+    link.href = `https://${username}.nekoweb.org/elements.css`;
+    document.getElementsByTagName("head")[0].appendChild(link);
   }
 }
+
+fetchSiteTitle();
+fillUserSiteboxData();
+fillSiteboxCSS();
