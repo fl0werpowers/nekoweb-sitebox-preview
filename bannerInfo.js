9cbabf2134f1e21b4b44b6b55fe7b25982ffd30c
fl0werpowers <47599466+fl0werpowers@users.noreply.github.com>
2024-03-27 08:57:19 +0100

    Remove ES6 stuff

--- bannerInfo.js
+++ bannerInfo.js
@@ -5,35 +5,35 @@ const username = "dimden";
 // THE MAGIC SAUCE
 // (be careful when touching)
 
-async function fetchSiteTitle() {
-  const request = await fetch(`https://nekoweb.org/api/site/info/${username}`);
-  const siteInfoJson = await request.json();
-
-  if (document.getElementById("NOTUSEDONNEKOWEBsitetitle")) {
-    document.getElementById(
-      "NOTUSEDONNEKOWEBsitetitle"
-    ).innerHTML = `${siteInfoJson.title}`;
-  }
+function fetchSiteTitle() {
+  // const request = await fetch(`https://nekoweb.org/api/site/info/${username}`);
+  var siteInfoJson;
+  const url = "https://nekoweb.org/api/site/info/" + username;
+  const request = new XMLHttpRequest();
+  request.overrideMimeType("application/json");
+  request.open("GET", url, true);
+  request.onload = function () {
+    siteInfoJson = JSON.parse(request.responseText);
+    if (document.getElementById("NOTUSEDONNEKOWEBsitetitle")) {
+      document.getElementById(
+        "NOTUSEDONNEKOWEBsitetitle"
+      ).innerHTML = `${siteInfoJson.title}`;
+    }
+  };
 }
 
 function fillUserSiteboxData() {
-  document.getElementById(
-    "NOTUSEDONNEKOWEBsitelink"
-  ).innerHTML = `${username}.nekoweb.org <span
-  style="position: relative !important"
-  class="follow"
-  >[+]</span
->`;
-  document.getElementById(
-    "NOTUSEDONNEKOWEBanchor"
-  ).href = `https://${username}.nekoweb.org/`;
-  document.getElementById(
-    "NOTUSEDONNEKOWEBsitescreenshot"
-  ).src = `https://nekoweb.org/screenshots/${username}/index_large.jpg`;
+  document.getElementById("NOTUSEDONNEKOWEBsitelink").innerHTML =
+    username +
+    '.nekoweb.org <span style="position: relative !important" class="follow">[+]</span>';
+  document.getElementById("NOTUSEDONNEKOWEBanchor").href =
+    "https://" + username + ".nekoweb.org/";
+  document.getElementById("NOTUSEDONNEKOWEBsitescreenshot").src =
+    "https://nekoweb.org/screenshots/" + username + "/index_large.jpg";
 }
 
 function fillSiteboxCSS() {
-  const stylesheet = `https://${username}.nekoweb.org/elements.css`;
+  const stylesheet = "https://" + username + ".nekoweb.org/elements.css";
 
   const cssId = "userCss";
 
@@ -41,7 +41,7 @@ function fillSiteboxCSS() {
     const link = document.createElement("link");
     link.id = cssId;
     link.rel = "stylesheet";
-    link.href = `https://${username}.nekoweb.org/elements.css`;
+    link.href = stylesheet;
     document.getElementsByTagName("head")[0].appendChild(link);
   }
 }
